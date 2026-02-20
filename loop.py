6893c8c07860f447d42038bf9eee801f7fd4b271
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-02-20 11:51:36 -0300

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,9 @@
+print("ACERTE O NOME  SECRETO!!!")
+
+lista_nomes = ['Matheus', 'Laura', 'André', 'Ricardo', 'Letícia']
+alvo = 'Matheus'
+
+for alvo in lista_nomes:
+    if lista_nomes == alvo:
+        print(f"Acertou o nome secreto: {alvo}")
+        break
