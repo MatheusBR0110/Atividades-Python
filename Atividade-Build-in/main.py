26991ec7cc175b688a2eaab8928c9e96ee8706c0
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-03-13 11:05:39 -0300

    Add files via upload

---
+++ Atividade-Build-in/main.py
@@ -0,0 +1,23 @@
+input = input("Digite aqui:")
+usuarios = ['Matheus', 'Gustavo', 'Eduardo', 'Joaquim', 'Gregory', 'Lucas']
+Matheus = ['idade: 18', 'peso: 55', 'altura: 1,78']
+Gustavo = ['idade: 17', 'peso: 46', 'altura: 1,70']
+Eduardo = ['idade: 17', 'peso: 56', 'altura: 1,72']
+Joaquim = ['idade: 18', 'peso: 61', 'altura: 1,80']
+Gregory = ['idade: 17', 'peso: 57', 'altura: 1,74']
+Lucas = ['idade: 17 ', 'peso: 85', 'altura: 1,70']
+
+if(input == 'Matheus'):
+    print(Matheus)
+elif(input == 'Gustavo'):
+     print(Gustavo)
+elif(input == 'Eduardo'):
+     print(Eduardo)
+elif(input == 'Joaquim'):
+     print(Joaquim)
+elif(input == 'Gregory'):
+     print(Gregory)
+elif(input == 'Lucas'):
+     print(Lucas)
+else:
+     print("erro")
